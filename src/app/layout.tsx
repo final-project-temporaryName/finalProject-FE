1b14fe77dfce8db2182fdbfc30faf0647579d6f6
Gyuho Lee <j980303@naver.com>
2024-01-24 14:12:03 +0900

    chore: react query 보일러 플레이트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import ReactQueryProviders from '@/utils/react-query-provider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Team Six",
-  description: "Team Six",
+  title: 'Team Six',
+  description: 'Team Six',
 };
 
 export default function RootLayout({
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProviders>{children}</ReactQueryProviders>
+      </body>
     </html>
   );
 }
