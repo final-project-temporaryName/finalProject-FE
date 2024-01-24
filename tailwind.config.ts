e1762364fd65c88f1a39909b6d760fc22a3c03cd
James Song <skdmlrh12@gmail.com>
2024-01-24 17:44:49 +0900

    chore: tailwind.config.ts 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-// const px0_50 = { ...Array.from(Array(51)).map((_, i) => `${i}px`) };
-// const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
-// const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
-// const px0_400 = { ...Array.from(Array(401)).map((_, i) => `${i}px`) };
+const createPxEntries = (size: number) => {
+  return {
+    0: '0',
+    ...Array.from(Array(size + 1)).reduce((accumulator, _, i) => {
+      return { ...accumulator, [`${i}`]: `${i / 16}rem` };
+    }),
+  };
+};
 
-type AccType = Record<string, string>;
+const PX_ENTRIES_10 = createPxEntries(10);
+const PX_ENTRIES_100 = createPxEntries(100);
+const PX_ENTRIES_1000 = createPxEntries(1000);
+
+/* type AccType = Record<string, string>;
 
 const range = (start: number, end: number): number[] => {
   let array = [];
@@ -15,87 +23,83 @@ const range = (start: number, end: number): number[] => {
   return array;
 };
 
-const pxToRem = (px: number, base = 16) => `${px / base}rem`;
+const pxToRem = (px: number, base = 16) => `${px / base}rem`; */
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    spacing: {
-      ...range(0, 2000).reduce((acc: AccType, px: number) => {
-        acc[`${px}pxr`] = pxToRem(px);
-        return acc;
-      }, {}),
-    },
-    fontSize: {
-      ...range(0, 2000).reduce((acc: AccType, px: number) => {
-        acc[`${px}pxr`] = pxToRem(px);
-        return acc;
-      }, {}),
-    },
+    borderWidth: PX_ENTRIES_10,
+    fontSize: PX_ENTRIES_100,
+    // spacing values are inherited by the 'padding', 'margin', 'height', 'maxHeight', 'flex-basis', 'gap', 'inset', 'space', 'translate', 'scrollMargin', 'scrollPadding', and 'textIndent'.
+    spacing: PX_ENTRIES_1000,
     fontWeight: {
-      light: "400",
-      normal: "500",
-      bold: "700",
+      light: '400',
+      normal: '500',
+      bold: '700',
     },
     borderRadius: {
-      sm: "0.25rem",
-      md: "0.375rem",
-      lg: "0.5rem",
-      full: "9999px",
+      sm: '0.25rem',
+      md: '0.375rem',
+      lg: '0.5rem',
+      full: '9999px',
     },
     colors: {
-      transparent: "transparent",
+      transparent: 'transparent',
       primary: {
-        DEFAULT: "rgb(var(--primary) / <alpha-value>)",
-        light: "rgb(var(--primary-light) / <alpha-value>)",
+        DEFAULT: 'rgb(var(--primary) / <alpha-value>)',
+        light: 'rgb(var(--primary-light) / <alpha-value>)',
       },
-      red: "rgb(var(--red) / <alpha-value>)",
+      red: 'rgb(var(--red) / <alpha-value>)',
       green: {
-        DEFAULT: "rgb(var(--green) / <alpha-value>)",
-        light: "rgb(var(--green-light) / <alpha-value>)",
+        DEFAULT: 'rgb(var(--green) / <alpha-value>)',
+        light: 'rgb(var(--green-light) / <alpha-value>)',
       },
       purple: {
-        DEFAULT: "rgb(var(--purple) / <alpha-value>)",
-        light: "rgb(var(--purple-light) / <alpha-value>)",
+        DEFAULT: 'rgb(var(--purple) / <alpha-value>)',
+        light: 'rgb(var(--purple-light) / <alpha-value>)',
       },
       orange: {
-        DEFAULT: "rgb(var(--orange) / <alpha-value>)",
-        light: "rgb(var(--orange-light) / <alpha-value>)",
+        DEFAULT: 'rgb(var(--orange) / <alpha-value>)',
+        light: 'rgb(var(--orange-light) / <alpha-value>)',
       },
       blue: {
-        DEFAULT: "rgb(var(--blue) / <alpha-value>)",
-        light: "rgb(var(--blue-light) / <alpha-value>)",
-        hoverLight: "rgb(var(--blue-hover-light) / <alpha-value>)",
+        DEFAULT: 'rgb(var(--blue) / <alpha-value>)',
+        light: 'rgb(var(--blue-light) / <alpha-value>)',
+        hoverLight: 'rgb(var(--blue-hover-light) / <alpha-value>)',
       },
       pink: {
-        DEFAULT: "rgb(var(--pink) / <alpha-value>)",
-        light: "rgb(var(--pink-light) / <alpha-value>)",
+        DEFAULT: 'rgb(var(--pink) / <alpha-value>)',
+        light: 'rgb(var(--pink-light) / <alpha-value>)',
       },
-      black: "rgb(var(--black) / <alpha-value>)",
-      white: "rgb(var(--white) / <alpha-value>)",
-      WHITE: "rgb(var(--WHITE) / <alpha-value>)",
+      black: 'rgb(var(--black) / <alpha-value>)',
+      white: 'rgb(var(--white) / <alpha-value>)',
+      WHITE: 'rgb(var(--WHITE) / <alpha-value>)',
       gray: {
-        1: "rgb(var(--gray-1) / <alpha-value>)",
-        2: "rgb(var(--gray-2) / <alpha-value>)",
-        3: "rgb(var(--gray-3) / <alpha-value>)",
-        4: "rgb(var(--gray-4) / <alpha-value>)",
-        5: "rgb(var(--gray-5) / <alpha-value>)",
-        6: "rgb(var(--gray-6) / <alpha-value>)",
-        7: "rgb(var(--gray-7) / <alpha-value>)",
+        1: 'rgb(var(--gray-1) / <alpha-value>)',
+        2: 'rgb(var(--gray-2) / <alpha-value>)',
+        3: 'rgb(var(--gray-3) / <alpha-value>)',
+        4: 'rgb(var(--gray-4) / <alpha-value>)',
+        5: 'rgb(var(--gray-5) / <alpha-value>)',
+        6: 'rgb(var(--gray-6) / <alpha-value>)',
+        7: 'rgb(var(--gray-7) / <alpha-value>)',
       },
     },
     screens: {
       // tablet: { max: '1279px' },
       // => @media (max-width: 1279px) { ... }
 
-      mobile: { max: "375px" },
+      mobile: { max: '375px' },
       // => @media (max-width: 375px) { ... }
     },
-    extend: {},
+    extend: {
+      height: {
+        screen: '100dvh', //dvh를 애용합시다!
+      },
+    },
   },
   plugins: [],
 };
