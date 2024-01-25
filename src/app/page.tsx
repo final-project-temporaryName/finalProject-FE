780af603ea8bfd2c6954217b007488fda3882120
K-Y-Hoo <bookopen1@gmail.com>
2024-01-25 20:00:16 +0900

    Feat: Card 컴포넌트의 임시 값 제거, props 값 연동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,16 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       main
       <p>dev 브랜치 자동 배포 테스트용 텍스트입니다..!</p>
-      <Card />
+      <Card
+        onSale={true}
+        workTitle="작품 제목"
+        authorName="작가 닉네임"
+        likeCount={1}
+        viewCount={1}
+        commentCount={1}
+        workImageUrl="https://images.unsplash.com/photo-1579273168855-c63546c129dd?q=80&w=1355&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        profileImageUrl="https://images.unsplash.com/photo-1579273168855-c63546c129dd?q=80&w=1355&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
       <p className="font-light">test</p>
     </main>
   );
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import OnSaleImage from '../../public/assets/images/OnSaleImage.png';
-import SampleImage from '../../public/assets/images/CardSampleImage.png';
 
 interface CardProps {
   onSale: boolean;
@@ -13,23 +12,34 @@ interface CardProps {
   authorName: string;
 }
 
-function Card() {
+function Card({
+  onSale,
+  workImageUrl,
+  workTitle,
+  likeCount,
+  viewCount,
+  commentCount,
+  profileImageUrl,
+  authorName,
+}: CardProps) {
   return (
     <div className="flex flex-col w-280 h-328">
       <div className="relative">
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black"></div>
-        <div className="absolute right-17">
-          <Image src={OnSaleImage} alt="판매중 이미지" width={29} height={56} />
-        </div>
-        <Image src={SampleImage} alt="카드 이미지" width={280} height={280} />
+        {onSale && (
+          <div className="absolute right-17">
+            <Image src={OnSaleImage} alt="판매중 이미지" width={29} height={56} />
+          </div>
+        )}
+        <Image className="w-280 h-280" src={workImageUrl} alt="카드 이미지" width={280} height={280} />
         <div className="flex justify-between w-280 h-10 px-15 absolute bottom-25">
-          <p className="text-base font-normal font-semibold leading-normal text-white">작품 제목</p>
+          <p className="text-base font-normal font-semibold leading-normal text-white">{workTitle}</p>
           <div>좋아요 컴포넌트</div>
         </div>
       </div>
       <div className="flex items-center bg-primary-1 w-280 h-48 gap-10 pt-5">
-        <Image className="rounded-full" src={SampleImage} alt="프로필 이미지" width={40} height={40} />
-        <p className="text-base font-normal font-bold leading-normal">작가 닉네임</p>
+        <Image className="rounded-full w-40 h-40" src={profileImageUrl} alt="프로필 이미지" width={40} height={40} />
+        <p className="text-base font-normal font-bold leading-normal">{authorName}</p>
       </div>
     </div>
   );
