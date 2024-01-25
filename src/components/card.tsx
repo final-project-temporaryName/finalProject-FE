8039edf9c42cc14392c2cc3fed39dded07e1eaf4
K-Y-Hoo <bookopen1@gmail.com>
2024-01-25 19:35:37 +0900

    Style: Card 컴포넌트 디자인 구현

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,17 +1,25 @@
-import SampleImage from '../../public/assets/images/CardSampleImage.png';
 import Image from 'next/image';
+import OnSaleImage from '../../public/assets/images/OnSaleImage.png';
+import SampleImage from '../../public/assets/images/CardSampleImage.png';
 
 function Card() {
   return (
     <div className="flex flex-col w-280 h-328">
-      <div>
+      <div className="relative">
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black"></div>
+        <div className="absolute right-17">
+          <Image src={OnSaleImage} alt="판매중 이미지" width={29} height={56} />
+        </div>
         <Image src={SampleImage} alt="카드 이미지" width={280} height={280} />
-        <div>
-          <div></div>
-          <div></div>
+        <div className="flex justify-between w-280 h-10 px-15 absolute bottom-25">
+          <p className="text-base font-normal font-semibold leading-normal text-white">작품 제목</p>
+          <div>좋아요 컴포넌트</div>
         </div>
       </div>
-      <div className="flex items-center bg-primary-1 w-280 h-48">작가 닉네임</div>
+      <div className="flex items-center bg-primary-1 w-280 h-48 gap-10 pt-5">
+        <Image className="rounded-full" src={SampleImage} alt="프로필 이미지" width={40} height={40} />
+        <p className="text-base font-normal font-bold leading-normal">작가 닉네임</p>
+      </div>
     </div>
   );
 }
