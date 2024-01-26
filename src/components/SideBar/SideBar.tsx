9ab20925f31e6ed37b3a7db8ba3c80b6bb1d1d23
Gyuho Lee <j980303@naver.com>
2024-01-26 19:04:51 +0900

    feat: pull 받은 것 추가

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -44,7 +44,7 @@ const SideBar: React.FC<Props> = ({ name, role, description, likes, followers, i
               좋아요&nbsp;&nbsp;<span className="text-14 font-bold">{likes}</span>&nbsp;개
             </span>
             <span className="count">
-              팔로워&nbsp;&nbsp;<span className="text-14 font-bold">{followers}</span>&nbsp;명
+              팔로워 &nbsp;&nbsp;<span className="text-14 font-bold">{followers}</span>&nbsp;명
             </span>
           </div>
           <Link href="/내정보수정" className="mt-30 flex w-116 items-center gap-4 text-12 text-gray-9">
