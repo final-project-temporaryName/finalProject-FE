e6e89d6a271d455a2df157283d0bdc013c665800
K-Y-Hoo <bookopen1@gmail.com>
2024-01-26 13:38:05 +0900

    env: page.tsx 내부 임시 값 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,8 @@
-import Card from '@/components/card/card';
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       main
       <p>dev 브랜치 자동 배포 테스트용 텍스트입니다..!</p>
-      <Card
-        onSale={true}
-        workTitle="작품 제목"
-        authorName="작가 닉네임"
-        likeCount={1200}
-        viewCount={999}
-        commentCount={0}
-        workImageUrl="https://images.unsplash.com/photo-1579273168855-c63546c129dd?q=80&w=1355&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        profileImageUrl="https://images.unsplash.com/photo-1579273168855-c63546c129dd?q=80&w=1355&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        workUrl="/"
-        authorUrl="/"
-      />
       <p className="font-light">test</p>
     </main>
   );
