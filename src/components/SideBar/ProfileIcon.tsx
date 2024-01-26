d6deef09f18b1e0155c8f1c760fcacc622710224
Gyuho Lee <j980303@naver.com>
2024-01-26 13:47:17 +0900

    Feat: SideBar 컴포넌트 구현

---
+++ src/components/SideBar/ProfileIcon.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import DefaultProfile from '../../../public/Icons/default_profile.svg';
+
+function ProfileIcon() {
+  return (
+    <div>
+      <DefaultProfile />
+    </div>
+  );
+}
+
+export default ProfileIcon;
