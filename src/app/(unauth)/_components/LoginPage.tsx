e16d4e467807a67be27d1f7e532c978a66635674
Ssong-Q <skdmlrh12@gmail.com>
2024-01-30 02:34:48 +0900

    feat: 로그인 시 리다이렉트 구현 및 비즈니스 계정으로 전환하여 email, name 정보 수집

--- src/app/(unauth)/_components/LoginPage.tsx
+++ src/app/(unauth)/_components/LoginPage.tsx
@@ -8,19 +8,21 @@ function LoginPage() {
   return (
     <>
       <div className="w-dvh flex h-dvh flex-col justify-start">
-        <Image src={loginBgImg} alt="로그인 페이지 사진" className="w-dvh h-[65%]" />
-        <article className="absolute top-[65%] flex w-381 flex-col items-center justify-start gap-28">
-          <div className="w-full flex-col items-center gap-4">
-            <h1 className="text-32 font-bold">반가워요✨</h1>
+        <div className="w-dvh relative h-[65%]">
+          <Image src={loginBgImg} alt="로그인 페이지 사진" fill priority />
+          <div className="absolute bottom-30 w-full flex-col items-center gap-4">
+            <h1 className="text-center text-32 font-bold">반가워요✨</h1>
             <br />
-            <p className="text-16">
+            <p className="text-center text-16">
               <span className="font-bold text-primary">청춘예찬</span>을 통해 여러 아티스트들과 소통하고
               <br />
               작품거래 및 공유로 트렌드를 만들어 보세요!
             </p>
           </div>
+        </div>
+        <div className="flex justify-center pt-30">
           <KakaoLoginButton />
-        </article>
+        </div>
       </div>
     </>
   );
