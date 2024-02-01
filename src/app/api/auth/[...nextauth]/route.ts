39980c3163b821646fa318fddc27fd7deeb799bb
K-Y-Hoo <bookopen1@gmail.com>
2024-02-01 16:25:17 +0900

    fix: build 에러 유발 코드 수정

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,9 +3,14 @@ import { NextAuthOptions, Account, Profile, Session, DefaultSession } from 'next
 import { JWT } from 'next-auth/jwt';
 import { AdapterUser } from 'next-auth/adapters';
 import GoogleProvider from 'next-auth/providers/google';
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 
 interface User {
   id: string;
+}
+
+interface ErrorResponse {
+  error: string;
 }
 
 interface MyNextAuthOptions {
@@ -32,24 +37,45 @@ interface MyNextAuthOptions {
   };
 }
 
-const handler: MyNextAuthOptions = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    }),
-  ],
-  callbacks: {
-    async jwt(params) {
-      // console.log(params?.token?.sub);
-      // 이 라인에 백엔드 서버에 POST 요청을 보낼 함수를 작성하면 됩니다.
-      // params?.token?.sub이 unique key 값입니다.
-      return params.token;
-    },
-    async session(params) {
-      return params.session;
-    },
-  },
-});
+const handler: NextApiHandler<MyNextAuthOptions | ErrorResponse> = async (
+  req: NextApiRequest,
+  res: NextApiResponse<MyNextAuthOptions | ErrorResponse>,
+) => {
+  try {
+    const result = await NextAuth(req, res, {
+      providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID || '',
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+        }),
+      ],
+      callbacks: {
+        async jwt(params) {
+          // console.log(params?.token?.sub);
+          // 이 라인에 백엔드 서버에 POST 요청을 보낼 함수를 작성하면 됩니다.
+          // params?.token?.sub이 unique key 값입니다.
+          return params.token;
+        },
+        async session(params) {
+          return params.session;
+        },
+      },
+    });
 
-export { handler as GET, handler as POST };
+    // 핸들러에서 응답을 반환
+    return result;
+  } catch (error) {
+    // 오류가 발생한 경우에도 응답을 반환
+    const errorMessage = 'Internal Server Error';
+    res.status(500).json({ error: errorMessage });
+  }
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export const GET = handler;
+export const POST = handler;
