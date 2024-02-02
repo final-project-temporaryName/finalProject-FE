f6b05f6002f1a54e583a6145f79a79602abdb817
K-Y-Hoo <bookopen1@gmail.com>
2024-02-02 11:42:58 +0900

    feat: 상단 네비게이션 바 값 삭제 버튼 구현

--- src/components/NavBar/SearchBar.tsx
+++ src/components/NavBar/SearchBar.tsx
@@ -3,13 +3,15 @@
 import { useStore } from '@/store';
 import { useForm } from 'react-hook-form';
 import SearchIcon from '../../../public/assets/icons/search.svg';
+import CancelIcon from '../../../public/assets/icons/CancelIcon.svg';
+import { MouseEvent } from 'react';
 
 interface IForm {
   query?: string;
 }
 
 function SearchBar() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const { clearSearchWord, setSearchWord } = useStore((state) => ({
     setSearchWord: state.setSearchWord,
     clearSearchWord: state.clearSearchWord,
@@ -20,6 +22,12 @@ function SearchBar() {
     else setSearchWord(data.query.trim());
   };
 
+  const handleClearClick = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    reset();
+    clearSearchWord();
+  };
+
   return (
     <form className="navSearchBar" onSubmit={handleSubmit(onValid)}>
       <input
@@ -28,6 +36,9 @@ function SearchBar() {
         placeholder="#감성도자기  #수상작"
         {...register('query')}
       />
+      <button type="button" onClick={(e) => handleClearClick(e)}>
+        <CancelIcon />
+      </button>
       <button title="Submit" type="submit">
         <SearchIcon />
       </button>
