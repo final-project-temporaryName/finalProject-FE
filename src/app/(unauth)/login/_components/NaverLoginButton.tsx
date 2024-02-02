4d123a60e907275dacbc1dcc7791038acaa574c5
Ssong-Q <skdmlrh12@gmail.com>
2024-02-02 12:03:35 +0900

    feat: 리다이렉트 작업 진행

--- src/app/(unauth)/login/_components/NaverLoginButton.tsx
+++ src/app/(unauth)/login/_components/NaverLoginButton.tsx
@@ -4,25 +4,17 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { MouseEvent } from 'react';
 import naverLogoImg from '../../../../../public/assets/images/naverLogo.png';
-import { useStore } from '@/store';
 
 function NaverLoginButton() {
   const { data: session } = useSession();
-  const { setLogin, setLogout } = useStore((state) => ({
-    setLogin: state.setLogin,
-    setLogout: state.setLogout,
-  }));
 
   const handleNaverLoginClick = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setLogin();
 
     if (session) {
       // 추후 로그아웃 기능 삭제 예정
-      setLogout();
       await signOut();
     } else {
-      setLogin();
       await signIn('naver', { redirect: true, callbackUrl: '/login/postFlow' });
     }
   };
