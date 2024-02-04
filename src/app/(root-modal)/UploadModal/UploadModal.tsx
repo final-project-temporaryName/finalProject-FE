b1bdf91fd2e92fde1380610f4667085f20e0c2bb
BeMatthewsong <songww1997@naver.com>
2024-02-04 23:29:07 +0900

    design: 게시물 업로드 오른쪽 부분 추가 (리액트 퀼 작동 안 됨)

--- src/app/(root-modal)/UploadModal/UploadModal.tsx
+++ src/app/(root-modal)/UploadModal/UploadModal.tsx
@@ -1,13 +1,55 @@
 'use client';
 
+import PrimaryButton from '@/components/Button/PrimaryButton';
 import '@/styles/tailwind.css';
+import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
+import { useMemo, useState } from 'react';
+import 'react-quill/dist/quill.snow.css';
 import UploadImage from '../../../../public/assets/icons/cloud_upload.svg';
 import Modal from '../_components';
+import Image from 'next/image';
+import saleLable from '../../../../public/assets/images/OnSaleImage.png';
+
+// document가 정의되기 전에 react-quill이 로드 되고, 정의되지 않은 document를 조작하려고 해서 에러가 발생
+// 그래서 lazy-load하기 위해 dynamic import 사용
+const QuillNoSSRWrapper = dynamic(import('react-quill'), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+});
 
 // TODO: 이미지에 따른 조건부 렌더링, API 함수 붙이기
 export default function UploadModal() {
+  const [description, setDescription] = useState('');
   const router = useRouter();
+
+  const modules = useMemo(
+    () => ({
+      toolbar: [
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+      ],
+      clipboard: {
+        matchVisual: false,
+      },
+    }),
+    [],
+  );
+
+  const formats = [
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'align',
+    'image',
+  ];
 
   const onClickClose = () => {
     router.back();
@@ -15,6 +57,10 @@ export default function UploadModal() {
 
   const onClickImageUpload = () => {
     console.log('사진 업로드 API 함수 호출');
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setDescription(e.target.value);
   };
 
   return (
@@ -35,13 +81,40 @@ export default function UploadModal() {
             </button>
           </div>
         </div>
-        <div className="h-full w-full p-20">
+        <div className="relative flex h-full w-full flex-col gap-18 p-20">
           <input
             className="h-39 w-300 p-10 placeholder:text-gray-5"
             type="text"
             spellCheck="false"
             placeholder="게시글 제목을 입력해주세요"
           />
+          <div className="absolute right-5 top-0 gap-13">
+            <div className="flex items-center justify-center">
+              <Image src={saleLable} alt="세일 라벨" width={30} height={56} />
+            </div>
+            <div>
+              <div className="status-label">게시용</div>
+              <div className="status-label">판매용</div>
+              <div className="status-label">나눔용</div>
+            </div>
+          </div>
+          <div className="h-333 w-355 p-10">
+            {/* value, onChange, ref 추가 */}
+            <QuillNoSSRWrapper
+              theme="snow"
+              id={'description'}
+              modules={modules}
+              formats={formats}
+              value={description}
+              placeholder={'작품을 어필해보세요!'}
+              onChange={() => handleChange}
+            />
+          </div>
+          <div className="flex justify-end">
+            <PrimaryButton destination={'/mypage'} classname={'save-button'}>
+              저장하기
+            </PrimaryButton>
+          </div>
         </div>
       </Modal.Body>
     </Modal.Container>
