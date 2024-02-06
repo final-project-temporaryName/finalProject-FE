28a6c1db2babdfb71593d9b7bfe8752a4cea882f
Ssong-Q <skdmlrh12@gmail.com>
2024-02-06 23:36:12 +0900

    chore: 기존 코드에서 axios 부분 삭제

--- src/api/auth/postUserId.ts
+++ src/api/auth/postUserId.ts
@@ -1,15 +1,3 @@
-import instance from '@/lib/axios';
-import { AxiosError, isAxiosError } from 'axios';
-
 export const postUserId = async (id: string | undefined) => {
-  try {
-    const response = await instance.post(`/user/id`, {
-      id: id,
-    });
-    return response.data;
-  } catch (error) {
-    if (isAxiosError<AxiosError>(error)) {
-      return error.response?.data.message;
-    }
-  }
+  // fetch로 구현 예정
 };
