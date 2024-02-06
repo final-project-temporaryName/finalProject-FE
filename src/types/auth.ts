dc202150f12493b114d1f4f858f8e1939631269e
Ssong-Q <skdmlrh12@gmail.com>
2024-02-06 22:53:54 +0900

    chore: auth, cards, users 타입 수정 및 생성 완료

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,1 +1,13 @@
 // 로그인, 회원가입 관련 타입
+
+export interface GetMyToken {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface PostSnSLoginInfo {
+  snsUniqueId: string;
+  snsType: string;
+}
+
+// 닉네임 중복 체크 API 는 nickname을 string query로
