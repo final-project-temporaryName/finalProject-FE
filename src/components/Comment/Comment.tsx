ba25b33561b3d62e648675b8e35ee13db3fc5ad5
K-Y-Hoo <bookopen1@gmail.com>
2024-02-07 20:54:38 +0900

    feat: 댓글 아이콘 클릭 시 댓글 컴포넌트 높이 변경 구현

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { MouseEvent, useState } from 'react';
 import Free from './Free';
 import Selling from './Selling';
+import WhiteComment from './WhiteComment';
+import BlackComment from './BlackComment';
 
 interface CommentProps {
   likeCount: number;
@@ -10,9 +13,30 @@ interface CommentProps {
 }
 
 function Comment({ likeCount, commentCount, artworkStatus }: CommentProps) {
+  const [isCommentClicked, setIsCommentClicked] = useState(false);
+
+  const handleCommentClick = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsCommentClicked((prev) => !prev);
+  };
+
   return (
-    <div className="shadow-top relative w-full min-w-400 rounded-t-sm bg-primary-1">
-      댓글 컴포넌트
+    <div
+      className={
+        isCommentClicked
+          ? 'shadow-top relative h-500 w-full min-w-400 rounded-t-sm bg-primary-1'
+          : 'shadow-top relative h-50 w-full min-w-400 rounded-t-sm bg-primary-1'
+      }
+    >
+      <button
+        className="flex items-center"
+        onClick={(e) => {
+          handleCommentClick(e);
+        }}
+      >
+        {isCommentClicked ? <WhiteComment /> : <BlackComment />}
+        <p className="mb-3 text-12">댓글 {commentCount}</p>
+      </button>
       <div className="absolute right-20" style={{ top: '-10px' }}>
         {artworkStatus === 'SELLING' ? <Selling /> : artworkStatus === 'FREE' ? <Free /> : null}
       </div>
