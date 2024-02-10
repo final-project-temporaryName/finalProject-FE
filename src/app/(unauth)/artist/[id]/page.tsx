e21b8fb65140190fa0284cbab429dde8d2c240db
K-Y-Hoo <bookopen1@gmail.com>
2024-02-10 20:36:27 +0900

    feat: 작가 상세 페이지 카테고리 버튼 구현

--- src/app/(unauth)/artist/[id]/page.tsx
+++ src/app/(unauth)/artist/[id]/page.tsx
@@ -1,11 +1,10 @@
-import CategoryButtonGroup from '@/components/Button/Category/CategoryButtonGroup';
-import CardContainer from '@/components/Card/CardContainer';
 import NavBar from '@/components/NavBar/NavBar';
 import NavigatorBox from '@/components/NavBar/NavigatorBox';
 import SideBar from '@/components/SideBar/SideBar';
+import ArtistCardSection from '../../_components/Artist/ArtistCardSection';
 
 export default function Artist() {
-  // navBar와 sideBar에 들어갈 id값은 페이지의 주소 query를 받아서 전달해야함
+  // sideBar에 들어갈 id값은 페이지의 주소 query를 받아서 전달해야함
   return (
     <>
       <NavBar>
@@ -13,8 +12,7 @@ export default function Artist() {
       </NavBar>
       <SideBar id={2} displayStatus="notMyWork" />
       <main className="ml-330 mt-157">
-        <CategoryButtonGroup />
-        <CardContainer />
+        <ArtistCardSection />
       </main>
     </>
   );
