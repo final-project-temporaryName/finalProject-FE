dc4dcf5603e85e8a73c14a8c1fa6fdb697ea997f
K-Y-Hoo <bookopen1@gmail.com>
2024-02-10 20:06:18 +0900

    feat: 작가 상세 페이지 sideBar 컴포넌트 내부 재배치

--- src/app/(unauth)/artist/[id]/page.tsx
+++ src/app/(unauth)/artist/[id]/page.tsx
@@ -1,3 +1,21 @@
+import CategoryButtonGroup from '@/components/Button/Category/CategoryButtonGroup';
+import CardContainer from '@/components/Card/CardContainer';
+import NavBar from '@/components/NavBar/NavBar';
+import NavigatorBox from '@/components/NavBar/NavigatorBox';
+import SideBar from '@/components/SideBar/SideBar';
+
 export default function Artist() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">작가 상세 페이지</main>;
+  // navBar와 sideBar에 들어갈 id값은 페이지의 주소 query를 받아서 전달해야함
+  return (
+    <>
+      <NavBar>
+        <NavigatorBox />
+      </NavBar>
+      <SideBar id={2} displayStatus="notMyWork" />
+      <main className="ml-330 mt-157">
+        <CategoryButtonGroup />
+        <CardContainer />
+      </main>
+    </>
+  );
 }
