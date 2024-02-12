c9a637ede84dfc71bd55f53e2e13584670be9d40
K-Y-Hoo <bookopen1@gmail.com>
2024-02-12 21:20:11 +0900

    fix: 확대 취소 버튼 위치 변경

--- src/components/SlideContainer/SlideContainer.tsx
+++ src/components/SlideContainer/SlideContainer.tsx
@@ -64,11 +64,12 @@ function SlideContainer({ imageUrlList }: SlideContainerProps) {
       {showModal && (
         <>
           <div className="fixed left-0 top-0 z-infinite h-full w-full bg-black opacity-65"></div>
+
           <div
             className="!important fixed left-0 top-0 z-infinite flex h-full w-full items-center justify-center"
             onClick={closeModal}
           >
-            <button className="absolute right-400 top-150 text-black" onClick={closeModal}>
+            <button className="absolute right-400 top-100 z-infinite" onClick={closeModal}>
               <Quit />
             </button>
             <Image src={selectedImage} alt="작품 확대 이미지" width={750} height={900} />
