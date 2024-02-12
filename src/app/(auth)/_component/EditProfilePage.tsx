46fb76e271dd1b31cbed4c0bc5754a3f4c967c6f
Gyuho Lee <j980303@naver.com>
2024-02-12 17:48:37 +0900

    feat: NavigatorBox 추가

--- src/app/(auth)/_component/EditProfilePage.tsx
+++ src/app/(auth)/_component/EditProfilePage.tsx
@@ -1,10 +1,13 @@
 import NavBar from '@/components/NavBar/NavBar';
+import NavigatorBox from '@/components/NavBar/NavigatorBox';
 import ProfilePage from '@/components/ProfilePage/ProfilePage';
 
 function EditProfilePage() {
   return (
     <div className="h-100dvh w-100dvw bg-white">
-      <NavBar />
+      <NavBar>
+        <NavigatorBox />
+      </NavBar>
       <ProfilePage type="put" />
     </div>
   );
