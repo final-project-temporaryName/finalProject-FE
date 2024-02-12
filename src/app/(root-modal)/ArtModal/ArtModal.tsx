2e444db28308b6c9d47e5625809e96af6dec062b
K-Y-Hoo <bookopen1@gmail.com>
2024-02-12 16:24:43 +0900

    feat: slide 컴포넌트 틀 구현

--- src/app/(root-modal)/ArtModal/ArtModal.tsx
+++ src/app/(root-modal)/ArtModal/ArtModal.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import Modal from '../_components';
 import { useStore } from '@/store';
 import CommentContainer from '@/components/Comment/CommentContainer';
+import SlideContainer from '@/components/SlideContainer/SlideContainer';
 
 export default function ArtModal() {
   const router = useRouter();
@@ -19,31 +20,12 @@ export default function ArtModal() {
   return (
     <Modal.Container onClickClose={onClickClose} classname="artModalContainer">
       <Modal.ArtHeader onClickClose={onClickClose} />
-      <Modal.Body classname="h-full">
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
-        <p>1</p>
+      <Modal.Body classname="h-full text-[#8f8f8f] p-10">
+        <SlideContainer />
+        <div className="flex flex-col gap-20 p-10 pt-20">
+          <p>게시글 제목</p>
+          <p>게시글 내용</p>
+        </div>
       </Modal.Body>
       <CommentContainer likeCount={1100} commentCount={3} artworkStatus="SELLING" />
     </Modal.Container>
---
+++ src/components/SlideContainer/SlideContainer.tsx
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+import '@/styles/tailwind.css';
+
+function SlideContainer() {
+  return (
+    <>
+      <Swiper pagination={true} modules={[Pagination]} className="swiper-container">
+        <SwiperSlide className="">Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+
+export default SlideContainer;
