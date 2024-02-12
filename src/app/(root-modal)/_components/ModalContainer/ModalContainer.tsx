36c8b67c4157351c411ca1a494ecfef266d8ab35
K-Y-Hoo <bookopen1@gmail.com>
2024-02-12 20:07:10 +0900

    fix: z-index값 수정

--- src/app/(root-modal)/_components/ModalContainer/ModalContainer.tsx
+++ src/app/(root-modal)/_components/ModalContainer/ModalContainer.tsx
@@ -14,7 +14,7 @@ export default function ModalContainer({ onClickClose, classname, children }: Pr
 
   return (
     <div
-      className="z-1000 fixed bottom-0 left-0 right-0 top-0 flex h-full w-screen justify-center bg-[#00000066]"
+      className="fixed bottom-0 left-0 right-0 top-0 z-infinite flex h-full w-screen justify-center bg-[#00000066]"
       onClick={onClickClose}
     >
       <div className={`${classname}`} onClick={(e) => e.stopPropagation()}>
