b31856918748088c31d02615f6ce7050915416fc
Gyuho Lee <j980303@naver.com>
2024-02-12 17:00:31 +0900

    refactor: 함수명, 구조 수정

--- src/app/(auth)/_component/EditProfilePage.tsx
+++ src/app/(auth)/_component/EditProfilePage.tsx
@@ -1,12 +1,7 @@
 import NavBar from '@/components/NavBar/NavBar';
 import ProfilePage from '@/components/ProfilePage/ProfilePage';
-import { ReactNode } from 'react';
 
-interface Props {
-  children: ReactNode;
-}
-
-export default function Layout({ children }: Props) {
+function EditProfilePage() {
   return (
     <div className="h-100dvh w-100dvw bg-white">
       <NavBar />
@@ -14,3 +9,5 @@ export default function Layout({ children }: Props) {
     </div>
   );
 }
+
+export default EditProfilePage;
