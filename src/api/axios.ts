b90fca42a5e36c68175a0a243a43b6ac63dccbf4
Ssong-Q <skdmlrh12@gmail.com>
2024-02-15 01:43:52 +0900

    feat: axios interceptors 401 에러에 대해서 refreshToken 보내며 새로운 accessToken 재발급 구현

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import _ from 'lodash';
 
 const instance = axios.create({
   baseURL: 'http://youth-alb-1112492853.ap-northeast-2.elb.amazonaws.com',
@@ -22,6 +23,41 @@ instance.interceptors.request.use(
     }
   },
   (error) => {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  (Response) => {
+    return Response;
+  },
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+    if (status === 401) {
+      const originalRequest = config;
+      const userAuth = localStorage.getItem('store');
+
+      if (userAuth) {
+        const oldParsedUserAuth = JSON.parse(userAuth);
+        const newParsedUserAuth = _.cloneDeep(oldParsedUserAuth);
+        const { userAccessToken, userRefreshToken } = oldParsedUserAuth.state;
+        const { data } = await axios.post('/reissue', {
+          accessToken: userAccessToken,
+          refreshToken: userRefreshToken,
+        });
+        const { accessToken: newAccessToken } = data;
+        newParsedUserAuth.state.userAccessToken = newAccessToken;
+        localStorage.setItem('store', JSON.stringify(newParsedUserAuth));
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return axios(originalRequest);
+      }
+    }
+
     return Promise.reject(error);
   },
 );
