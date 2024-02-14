1462d2cf0f921d00d5a82f38af1db7e7a46855c6
Ssong-Q <skdmlrh12@gmail.com>
2024-02-15 00:22:03 +0900

    feat: axios interceptors headers에 토큰 실어서 req 구현

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,4 +4,26 @@ const instance = axios.create({
   baseURL: 'http://youth-alb-1112492853.ap-northeast-2.elb.amazonaws.com',
 });
 
+instance.interceptors.request.use(
+  (config) => {
+    const userAuth = localStorage.getItem('store');
+
+    if (!userAuth) {
+      return config;
+    } else {
+      const {
+        state: { userAccessToken, userRefreshToken },
+      } = JSON.parse(userAuth);
+
+      config.headers.Authorization = `Bearer ${userAccessToken}`;
+      config.headers['Authorization-refresh'] = `Bearer ${userRefreshToken}`;
+
+      return config;
+    }
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default instance;
--- src/app/(unauth)/login/_components/RedirectToHome.tsx
+++ src/app/(unauth)/login/_components/RedirectToHome.tsx
@@ -3,6 +3,7 @@
 import { postUserId } from '@/api/auth/postSocialInfo';
 import { useStore } from '@/store';
 import { useSession } from 'next-auth/react';
+import { LocaleRouteNormalizer } from 'next/dist/server/future/normalizers/locale-route-normalizer';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
@@ -10,9 +11,10 @@ export default async function RedirectToHome() {
   const router = useRouter();
   const { data: session } = useSession();
 
-  const { setUserAccessToken, setUserRefreshToken } = useStore((state) => ({
+  const { setUserAccessToken, setUserRefreshToken, setUserRole } = useStore((state) => ({
     setUserAccessToken: state.setUserAccessToken,
     setUserRefreshToken: state.setUserRefreshToken,
+    setUserRole: state.setUserRole,
   }));
 
   const socialId = session?.user.id;
@@ -20,13 +22,16 @@ export default async function RedirectToHome() {
 
   const handleUserId = async () => {
     try {
-      const { accessToken, refreshToken } = await postUserId(socialId, socialType);
+      const { accessToken, refreshToken, userRole } = await postUserId(socialId, socialType);
       setUserAccessToken(accessToken);
       setUserRefreshToken(refreshToken);
+      setUserRole(userRole);
     } finally {
       router.replace('/');
     }
   };
+
+  console.log(localStorage.getItem('store'));
 
   useEffect(() => {
     if (session) {
