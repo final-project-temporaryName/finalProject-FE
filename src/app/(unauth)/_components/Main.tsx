be48cbe929ece4bef8cebccf1ff7b1b6d8f32e39
Ssong-Q <skdmlrh12@gmail.com>
2024-02-17 23:12:56 +0900

    refactor: Navbar를 root-layout으로 이동

--- src/app/(unauth)/_components/Main.tsx
+++ src/app/(unauth)/_components/Main.tsx
@@ -1,12 +1,8 @@
-import InfiniteText from '@/components/InfiniteText';
-import NavBar from '@/components/NavBar/NavBar';
 import MainCardSection from './Main/MainCardSection';
 
 function Main() {
   return (
     <>
-      <InfiniteText text="ART TALK - TALK  🎉  SITE FOR THE ARTISTS  •  DESIGNERS  •  CREATORS  🙌  SHARE YOUR CREATIVITY  😎  " />
-      <NavBar />
       <MainCardSection />
     </>
   );
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,6 +7,7 @@ import { usePathname } from 'next/navigation';
 import logoImg from '../../../public/assets/images/logo.png';
 import NavigatorBox from './NavigatorBox';
 import SearchBar from './SearchBar';
+import InfiniteText from '../InfiniteText';
 
 function NavBar() {
   const pathname = usePathname();
@@ -14,17 +15,22 @@ function NavBar() {
   const firstPathname = pathnameArr[1];
 
   return (
-    <nav
-      className={`navBar ${pathname === '/' || firstPathname === 'upload' || firstPathname === 'flow' || firstPathname === 'art' ? 'sticky' : 'fixed'}`}
-    >
-      <div className="flex flex-grow items-center justify-start gap-60">
-        <Link href={'/'} className="shrink-0">
-          <Image src={logoImg} alt="아트 톡톡 로고" width={85} height={85} />
-        </Link>
-        <SearchBar />
-      </div>
-      <NavigatorBox />
-    </nav>
+    <>
+      {pathname === '/' && (
+        <InfiniteText text="ART TALK - TALK  🎉  SITE FOR THE ARTISTS  •  DESIGNERS  •  CREATORS  🙌  SHARE YOUR CREATIVITY  😎  " />
+      )}
+      <nav
+        className={`navBar ${pathname === '/' || firstPathname === 'upload' || firstPathname === 'flow' || firstPathname === 'art' ? 'sticky' : 'fixed'} ${pathname === '/login' || pathname === '/signup' ? 'hidden' : 'flex'}`}
+      >
+        <div className="flex flex-grow items-center justify-start gap-60">
+          <Link href={'/'} className="shrink-0">
+            <Image src={logoImg} alt="아트 톡톡 로고" width={85} height={85} />
+          </Link>
+          <SearchBar />
+        </div>
+        <NavigatorBox />
+      </nav>
+    </>
   );
 }
 
