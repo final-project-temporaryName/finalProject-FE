ff4f9f9201840ac3c3d266ea987ec91960f5e541
Ssong-Q <skdmlrh12@gmail.com>
2024-02-20 19:23:28 +0900

    feat: 준회원으로 마이페이지 이동 시 askForSignupModal 뜨게끔 구현

--- src/app/(root-modal)/AskForSignupModal/AskForSignupModal.tsx
+++ src/app/(root-modal)/AskForSignupModal/AskForSignupModal.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { Button } from '@/components/Button';
-import Modal from '../_components';
 import { useStore } from '@/store';
+import Modal from '../_components';
 
 export default function AskForSignupModal() {
   const clearModal = useStore((state) => state.clearModal);
 
   return (
-    <Modal.Container classname="askForSignupContainer">
+    <Modal.Container type={'back'} classname="askForSignupContainer">
       <Modal.Body classname="flex flex-col-center gap-15">
         <div>내 정보 입력 후 회원가입을 완료하세요🎨</div>
         <Button
