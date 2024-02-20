19ae0834ec377c7f2a8bb0387d7d6bb09f60090f
BeMatthewsong <songww1997@naver.com>
2024-02-20 12:37:40 +0900

    feat: 게시물 수정 API 함수 추가 및 타입 리팩토링

--- src/types/cards.ts
+++ src/types/cards.ts
@@ -42,26 +42,19 @@ export interface GetArtworkImageResponse {
   imageUrl: string;
 }
 
-// 확정 후 수정 예정
 export interface PostCardRequestType {
+  imageIds: number[];
   title: string;
   description: string;
-  tags: string[]; // 논의 필요
-  imageUrl: string;
-  status: 'PUBLIC' | 'SELLING' | 'FREE';
+  artworkStatus: 'PUBLIC' | 'SELLING' | 'FREE';
 }
 
-export interface PostCardResponseType extends CardType {}
-
-// 확정 후 수정 예정
 export interface PutCardRequestType {
+  artworkId: number;
+  imageIds: number[];
   title: string;
   description: string;
-  tags: string[]; // 논의 필요
-  imageUrl: string;
-  status: 'PUBLIC' | 'SELLING' | 'FREE';
+  artworkStatus: 'PUBLIC' | 'SELLING' | 'FREE';
 }
-
-export interface PutCardResponseType extends CardType {}
 
 export interface DeleteCardRequestType {}
